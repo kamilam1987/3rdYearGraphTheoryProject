74994747fcff0f5887c51b09f4c87a336627e4a0
kamilam1987 <kamilam1987@gmail.com>
2018-02-21 09:46:41 +0000

    finished the Shunting Yard Algorithm

--- main.go
+++ main.go
@@ -14,17 +14,48 @@ func intopost(infix string) string {
 	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
 	//Arrray of runes, initialized an arrayof runs as empty
-	postfix := []rune{} //rune-character on the screen diplayed in UTF
+	pofix := []rune{} //rune-character on the screen diplayed in UTF
 
 	//Stack which stores ooperators from the infix regular expression
 	s := []rune{}
 
 	//Loop over the input
+	//range(convert into array of runes using UTF)
 	for _, r := range infix {
+		switch {
+		//Brackets
+		case r == '(':
+			//open bracet at the end of the stack
+			s = append(s, r)
+		case r == ')':
+			//Pop of the stack until finds the opening bracket
+			//len(s)-1 last element in the stacl
+			for s[len(s)-1] != '(' {
+				pofix = append(pofix, s[len(s)-1]) //last element of s
+				s = s[:len(s)-1]                   //everything in s accept the last character
+			}
 
+			s = s[:len(s)-1]
+		//Current character that reads from infix is a special character
+		case specials[r] > 0:
+			//while stack still hasan element on it and the precedence of the current character that reaads is less or = the precedence of the top element of the stcak
+			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
+				//Takes of the stack and stick into pofix
+				pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+			}
+			//Less precendence then append current character onto the stack
+			s = append(s, r)
+		//Not a bracket or a special character
+		default:
+			//Takes a character end stick and the end of output
+			pofix = append(pofix, r)
+		}
 	}
-
-	return string(postfix)
+	//If enythin on the stack append to the output
+	for len(s) > 0 {
+		pofix, s = append(pofix, s[len(s)-1]), s[:len(s)-1]
+	}
+	return string(pofix)
 } //End of intopost function
 
 //Main function contains regular expression written in infix notation
