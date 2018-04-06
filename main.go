319279123f8d85c8843b9f1680bfce468ac0dda9
kamilam1987 <kamilam1987@gmail.com>
2018-04-06 17:28:22 +0100

    Added option to check if a regular string match the postfix expression

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 
 	switch option {
 	case 1:
-		//Show which option was choosen
-		fmt.Println("Option:", option)
+		//Shows which option was choosen
+		fmt.Println("Option: ", option)
 		//Promt user to enter the infix string
 		fmt.Print("Enter infix expression: ")
 		//Examples of Infix expressions:
@@ -33,18 +33,55 @@ func main() {
 		infixExpression, err := nfa.UserInput()
 		//Error handler
 		if err != nil {
-			fmt.Println("Error", err.Error())
+			fmt.Println("Error ", err.Error())
 			return
 		}
 		//Changes from infix to postfix
 		postfixExpression := shunt.Intopost(infixExpression)
 		//Prints out the result
-		fmt.Println("Postfix notation is:", postfixExpression)
+		fmt.Println("Postfix notation is: ", postfixExpression)
 		//a.b.c Answer: ab.c.
 		//(a.(b|d))* Answer: abd|.*
 		//a.(b|d).c* Answer: abd|.c*.
 
+	case 2:
+		//Shows which option was choosen
+		fmt.Println("Option: ", option)
+		//Promt user to enter the postfix string
+		fmt.Print("Enter postfix expression: ")
+		//Reads data from user
+		infixExpression, err := nfa.UserInput()
+		//Error handler
+		if err != nil {
+			fmt.Println("Error: ", err.Error())
+			return
+		}
+		//Promnt user to enter a regular string
+		//Examples:
+		//Postfix expression : ab.c*| Example of string: cccc
+		//Postfix expression :ab.c*| Example of string: abc
+		//Postfix expression :ab.c*| Example of string: ""
+
+		//Promt user to enter a regular string
+		fmt.Print("Enter a string to test if it matches the nfa: ")
+		userString, err := nfa.UserInput()
+		//Error handler
+		if err != nil {
+			fmt.Println("Error: ", err.Error())
+			return
+		} //End of if
+		//Checks if maches
+		if nfa.Pomatch(infixExpression, userString) == true {
+			//Prints result
+			fmt.Println("Regular string: ", userString, " matches the expression: ", infixExpression)
+
+			//Checks if doesn't mach
+		} else if nfa.Pomatch(infixExpression, userString) == false {
+			//Prints result
+			fmt.Print("Regular string: ", userString, " doesn't match : ", infixExpression)
+		} //End of else if
+
 	default:
 		fmt.Println("Enter one of the above options")
-	}
+	} //End of switch
 } //End of main
